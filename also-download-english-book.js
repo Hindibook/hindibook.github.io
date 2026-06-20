9e548d4005b4bbd8d1f5051416aa197983e70320
Hindibook <162822375+Hindibook@users.noreply.github.com>
2026-06-20 07:04:23 +0530

    Update also-download-english-book.js

--- also-download-english-book.js
+++ also-download-english-book.js
@@ -1,26 +1,44 @@
-var numPosts = 6;  // Number of links
+<ul id="also-read"></ul><script>
+var numPosts = 6;
 
 function showAlsoRead(data) {
-  var entries = data.feed.entry;
   var ul = document.getElementById("also-read");
 
-  // Shuffle posts randomly
-  entries.sort(() => 0.5 - Math.random());
+  if (!ul) return;
+  if (!data || !data.feed || !data.feed.entry) {
+    ul.innerHTML = "<li>No posts found.</li>";
+    return;
+  }
 
-  // Pick only numPosts
-  entries.slice(0, numPosts).forEach(entry => {
-    // Replace | with - in post title
-    var title = entry.title.$t.replace(/\|/g, " - ");
-    var link = entry.link.find(l => l.rel === "alternate").href;
-    var li = document.createElement("li");
-    li.innerHTML = '<a href="' + link + '" target="_blank">' + title + '</a>';
-    ul.appendChild(li);
+  var entries = data.feed.entry.slice();
+
+  entries.sort(function() {
+    return Math.random() - 0.5;
   });
+
+  var html = "";
+
+  for (var i = 0; i < Math.min(numPosts, entries.length); i++) {
+    var entry = entries[i];
+    var title = entry.title.$t.replace(/\|/g, " - ");
+    var link = "#";
+
+    for (var j = 0; j < entry.link.length; j++) {
+      if (entry.link[j].rel === "alternate") {
+        link = entry.link[j].href;
+        break;
+      }
+    }
+
+    html += '<li><a href="' + link + '">' + title + '</a></li>';
+  }
+
+  ul.innerHTML = html;
 }
 
-// Load blog feed dynamically
 (function() {
-  var script = document.createElement('script');
-  script.src = "https://www.holybooks.in/feeds/posts/default?alt=json";
+  var script = document.createElement("script");
+  script.src = "https://www.holybooks.in/feeds/posts/default?alt=json-in-script&callback=showAlsoRead&max-results=50";
   document.body.appendChild(script);
 })();
+</script>
